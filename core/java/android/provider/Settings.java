793b12e1e83b99b442aaad04ef77d4287644b90e
Jmz <jmztaylor@gmail.com>
2014-01-06 15:30:15 -0500

    Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    idoybh (DerpFest) edit: Add settings value to backup

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6509,6 +6509,16 @@ public final class Settings {
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /** @hide */
+        private static final Validator FORCE_EXPANDED_NOTIFICATIONS_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6657,6 +6667,7 @@ public final class Settings {
             AMBIENT_LIGHT_DURATION,
             AMBIENT_LIGHT_REPEAT_COUNT,
             AMBIENT_LIGHT_PULSE_FOR_ALL,
+            FORCE_EXPANDED_NOTIFICATIONS,
         };
 
         /**
@@ -7068,6 +7079,7 @@ public final class Settings {
             VALIDATORS.put(AMBIENT_LIGHT_DURATION, AMBIENT_LIGHT_DURATION_VALIDATOR);
             VALIDATORS.put(AMBIENT_LIGHT_REPEAT_COUNT, AMBIENT_LIGHT_REPEAT_COUNT_VALIDATOR);
             VALIDATORS.put(AMBIENT_LIGHT_PULSE_FOR_ALL, AMBIENT_LIGHT_PULSE_FOR_ALL_VALIDATOR);
+            VALIDATORS.put(FORCE_EXPANDED_NOTIFICATIONS, FORCE_EXPANDED_NOTIFICATIONS_VALIDATOR);
         }
 
         /**
